82627ebd31b4f52dac10ed43e1cc97be5d89bcee
Matthew Lilley <mklilley@users.noreply.github.com>
2020-04-05 18:44:54 +0100

    Create server.js
    Adds simple node.js server for making quick requests

---
+++ server.js
@@ -0,0 +1,20 @@
+// Simple node.js server for making quick requests
+// Code from https://blog.risingstack.com/your-first-node-js-http-server/
+
+const https = require('https')
+const port = 3000
+
+const requestHandler = (request, response) => {
+  console.log(request.url)
+  response.end('Hello Node.js Server!')
+}
+
+const server = http.createServer(requestHandler)
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+})
